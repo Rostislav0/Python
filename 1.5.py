39186dc562f344f7593afb5544424ccb1b29366c
Rostislav0 <109548781+Rostislav0@users.noreply.github.com>
2022-10-13 11:32:56 +0300

    Update 1.5.py

--- 1.5.py
+++ 1.5.py
@@ -1,42 +1,58 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import text_to_be_present_in_element as pe
-from selenium.webdriver.support.wait import WebDriverWait
-import time
-import math
-import pyperclip
 
 
+class TestAbs(unittest.TestCase):
+    def conn_brow(self):
+        self.browser = webdriver.Chrome()
 
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
+    def fill_form(self, link):
+        path_first_block = "//div[@class='first_block']"
+        path1 = path_first_block + "//input[contains(@class, 'first')]"
+        path2 = path_first_block + "//input[contains(@class, 'second')]"
+        path3 = path_first_block + "//input[contains(@class, 'third')]"
+
+        content = {'Ivan': path1,
+                   'Petrov': path2,
+                   'iv@mail.ru': path3}
+
+        self.conn_brow()
+        self.browser.implicitly_wait(5)
+        self.browser.get(link)
+
+        for c, input in content.items():
+            self.browser.find_element(By.XPATH, input).send_keys(c)
+
+        button = self.browser.find_element(By.CSS_SELECTOR, 'button.btn')
+        button.click()
+
+        welcome_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        return welcome_text
+
+    def test_reg1(self):
+        try:
+            link = 'http://suninjuly.github.io/registration1.html'
+            welcome_text = self.fill_form(link)
+
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                             "Should be equal text")
+
+        finally:
+            self.browser.quit()
+
+    def test_reg2(self):
+        try:
+            link = 'http://suninjuly.github.io/registration2.html'
+            welcome_text = self.fill_form(link)
+
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                             "Should be equal text")
+
+        finally:
+            self.browser.quit()
 
 
-browser = webdriver.Chrome()
-link = "http://suninjuly.github.io/explicit_wait2.html"
-
-try:
-    browser.get(link)
-
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-
-    price = WebDriverWait(browser, 15).until(pe((By.ID, 'price'), '100'))
-
-    button.click()
-
-    x = browser.find_element(By.CSS_SELECTOR, "span[id='input_value']").text
-    answer = calc(x)
-
-    input_answer = browser.find_element(By.ID, 'answer')
-    input_answer.send_keys(answer)
-
-    submit = browser.find_element(By.ID, "solve")
-    submit.click()
-
-    alert = browser.switch_to.alert
-    affirm = alert.text.split()[-1]
-    pyperclip.copy(affirm)
-    print(affirm)
-
-finally:
-    browser.quit()
+if __name__ == "__main__":
+    unittest.main()
