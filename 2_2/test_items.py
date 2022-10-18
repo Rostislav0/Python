48e8a451a4a663998bcac99ba486a51ac85a39a2
Rostislav0 <109548781+Rostislav0@users.noreply.github.com>
2022-10-18 10:27:04 +0300

    add assert

--- 2_2/test_items.py
+++ 2_2/test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_is_there_button(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    buttons = browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(buttons) > 0, 'button not found'
